1844a83ac7594f81779f7fd249fc887cd56aca98
YMuley <91797130+YMuley@users.noreply.github.com>
2024-01-02 19:06:25 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,6 +8,7 @@ resource "azurerm_public_ip" "public_ip" {
   location            = each.value.location == null ? var.default_values.location : each.value.location
   sku                 = each.value.sku_tier == "Global" ? "Standard" : each.value.sku
   sku_tier            = each.value.sku_tier == null ? "Regional" : each.value.sku_tier
+  zones               = each.value.zones
   allocation_method   = each.value.allocation_method
   domain_name_label   = each.value.domain_name_label
   tags                = each.value.tags == null ? var.default_values.tags : each.value.tags
